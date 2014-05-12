7deec9f67736c8c9da1136bf60fe20a85edb327a
Santiago Perez Torre <saantii.28@hotmail.com>
2014-05-12 22:29:42 +0200

    Agregado el script de compra

---
+++ Scripts/Creacion tabla de compra.sql
@@ -0,0 +1,15 @@
+drop table compra
+create table compra
+(
+codigo numeric(18,0) identity(0,1),
+cantidad numeric(18,0),
+fecha datetime,
+username nvarchar(255) default null,
+codigo_publicacion numeric(18,0) foreign key references publicacion (codigo),
+codigo_calificacion numeric(18,0) default null,
+)
+
+insert into compra
+([cantidad], [fecha], [codigo_publicacion])
+select Compra_Cantidad, Compra_Fecha, Publicacion_Cod from gd_esquema.Maestra
+where ISNULL(Compra_Cantidad, 0) != 0
