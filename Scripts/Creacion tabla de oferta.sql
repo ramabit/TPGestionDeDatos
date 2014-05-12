9a06af6b0708db2fe22d3c2dbb9994b085df379e
Santiago Perez Torre <saantii.28@hotmail.com>
2014-05-12 22:30:08 +0200

    Modificado el script de oferta
    Le agregue el atributo fecha que faltaba

--- Scripts/Creacion tabla de oferta.sql
+++ Scripts/Creacion tabla de oferta.sql
@@ -4,12 +4,13 @@ create table oferta
 codigo numeric(18,0) identity(0,1),
 monto numeric(18,0),
 gano_subasta bit default 0,
+fecha datetime,
 username nvarchar(255) default null,
 codigo_publicacion numeric(18,0) foreign key references publicacion (codigo),
 codigo_calificacion numeric(18,0) default null,
 )
 
 insert into oferta
-([monto], [codigo_publicacion])
-select Oferta_Monto, Publicacion_Cod from gd_esquema.Maestra
+([monto], [fecha], [codigo_publicacion])
+select Oferta_Monto, Oferta_Fecha, Publicacion_Cod from gd_esquema.Maestra
 where ISNULL(Oferta_Monto, 0) != 0
