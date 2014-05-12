a906cd86a9811cc4b081b58b2c609848beada21c
Santiago Perez Torre <saantii.28@hotmail.com>
2014-05-12 03:03:26 +0200

    Agregando script de tabla empresa

---
+++ Scripts/Creacion tabla de empresa.sql
@@ -0,0 +1,16 @@
+create table empresa
+(razon_social nvarchar(255),
+cuit nvarchar(50),
+fecha_creacion datetime,
+mail nvarchar(50),
+domicilio_calle nvarchar(100),
+domicilio_numero numeric(18,0),
+domicilio_piso numeric(18,0),
+domicilio_depto nvarchar(50),
+domicilio_codigo_postal nvarchar(50))
+
+INSERT INTO empresa
+   ( [razon_social], [cuit], [fecha_creacion], [mail], [domicilio_calle], [domicilio_numero], [domicilio_piso], [domicilio_depto], [domicilio_codigo_postal])
+select distinct Publ_Empresa_Razon_Social, Publ_Empresa_Cuit, Publ_Empresa_Fecha_Creacion, Publ_Empresa_Mail, Publ_Empresa_Dom_Calle, Publ_Empresa_Nro_Calle, Publ_Empresa_Piso, Publ_Empresa_Depto, Publ_Empresa_Cod_Postal 
+from gd_esquema.Maestra 
+where ISNULL(Publ_Empresa_Razon_Social, '') != ''
