7f039502004628a1deb42e042ce596360604519d
Santiago Perez Torre <saantii.28@hotmail.com>
2014-05-13 21:52:09 +0200

    Agregado el script de usuario

---
+++ Scripts/Creacion tabla usuario.sql
@@ -0,0 +1,10 @@
+IF OBJECT_ID('Usuario', 'U') IS NOT NULL
+DROP TABLE Usuario
+
+create table Usuario
+(
+username nvarchar(45),
+password nvarchar(45),
+habilitado bit default 1,
+-- PRIMARY KEY ()
+)
