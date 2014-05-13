60a15476b957a0335deaf8703f3a252d2d028861
ramabit <f.e.ramirez94@gmail.com>
2014-05-12 22:56:22 -0300

    Agregado script de funcionalidad

---
+++ Scripts/Creacion tabla funcionalidad.sql
@@ -0,0 +1,55 @@
+create table Funcionalidad
+(id int identity(1,1) primary key,
+nombre varchar(45) not null);
+
+insert into Funcionalidad
+(nombre)
+values ('Comprar');
+
+insert into Funcionalidad
+(nombre)
+values ('Ofertar');
+
+insert into Funcionalidad
+(nombre)
+values ('Vender');
+
+insert into Funcionalidad
+(nombre)
+values ('Calificar vendedor');
+
+insert into Funcionalidad
+(nombre)
+values ('Preguntar');
+
+insert into Funcionalidad
+(nombre)
+values ('Agregar rol');
+
+insert into Funcionalidad
+(nombre)
+values ('Deshabilitar rol');
+
+insert into Funcionalidad
+(nombre)
+values ('Editar rol');
+
+insert into Funcionalidad
+(nombre)
+values ('Crear usuario');
+
+insert into Funcionalidad
+(nombre)
+values ('Editar usuario');
+
+insert into Funcionalidad
+(nombre)
+values ('Habilitar usuario');
+
+insert into Funcionalidad
+(nombre)
+values ('Deshabilitar usuario');
+
+insert into Funcionalidad
+(nombre)
+values ('Generar factura');
