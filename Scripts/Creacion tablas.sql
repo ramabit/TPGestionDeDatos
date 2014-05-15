eef2e0ab494265085fb5ea1f9f5940cde17cbf79
Santiago Perez Torre <saantii.28@hotmail.com>
2014-05-15 22:13:13 +0200

    Modificaciòn de pregunta y tablas
    Agregue una fecha a la pregunta

--- Scripts/Creacion tablas.sql
+++ Scripts/Creacion tablas.sql
@@ -148,6 +148,7 @@ create table Pregunta
 id numeric(18,0),
 descripcion nvarchar(255) not null,
 respuesta nvarchar(255) default '',
+respuesta_fecha datetime default null,
 id_publicacion numeric(18,0),
 PRIMARY KEY (id),
 FOREIGN KEY (id_publicacion) REFERENCES Publicacion (id)
