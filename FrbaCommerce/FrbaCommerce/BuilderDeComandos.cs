94ebc3ab83901377e664465dd7d4d022eaac2322
Santiago Perez Torre <saantii.28@hotmail.com>
2014-05-28 20:34:40 +0200

    Agregado un builder de comandos
    Para crear los comandos usemos esta clase. Hace lo mismo que hacia antes
    pero ahora no nos tenemos que preocupar por crear conexiones

---
+++ FrbaCommerce/FrbaCommerce/BuilderDeComandos.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+
+namespace FrbaCommerce
+{
+    class BuilderDeComandos
+    {
+        private SqlCommand command { get; set; }
+        private ConexionDB conexion = new ConexionDB();
+
+        public SqlCommand Crear(string sqlTexto, IList<SqlParameter> parametros)
+        {
+            this.command = new SqlCommand();
+            this.command.CommandText = sqlTexto;
+            if (parametros != null)
+            {
+                foreach (SqlParameter parametro in parametros)
+                {
+                    this.command.Parameters.Add(parametro);
+                }
+            }
+            if (this.command.Connection == null) this.command.Connection = conexion.AbrirConexion();
+
+            return this.command;
+        }
+    }
+}
