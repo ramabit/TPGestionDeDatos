5e24d8d5b5ec93fe3581036b89e45e6f799f07e8
Santiago Perez Torre <saantii.28@hotmail.com>
2014-05-29 21:29:29 +0200

    Agrego los forms de Cliente
    Solamente las clases. Falta el diseño

---
+++ FrbaCommerce/FrbaCommerce/Abm Cliente/AgregarCliente.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace FrbaCommerce.ABM_Cliente
+{
+    public partial class AgregarCliente : Form
+    {
+        public AgregarCliente()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ FrbaCommerce/FrbaCommerce/Abm Cliente/ClienteForm.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace FrbaCommerce.ABM_Cliente
+{
+    public partial class ClienteForm : Form
+    {
+        public ClienteForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
