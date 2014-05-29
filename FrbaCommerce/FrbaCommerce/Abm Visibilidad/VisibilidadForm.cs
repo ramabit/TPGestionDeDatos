45009e2c74f59bb92ac3a0dfb26c2f4c63a4b88d
Santiago Perez Torre <saantii.28@hotmail.com>
2014-05-29 21:30:56 +0200

    Agrego los forms de Visibilidad
    Las clases estan vacias. Falta el diseño tambien

---
+++ FrbaCommerce/FrbaCommerce/Abm Visibilidad/VisibilidadForm.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace FrbaCommerce.ABM_Visibilidad
+{
+    public partial class VisibilidadForm : Form
+    {
+        public VisibilidadForm()
+        {
+            InitializeComponent();
+        }
+
+        private void VisibilidadForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
