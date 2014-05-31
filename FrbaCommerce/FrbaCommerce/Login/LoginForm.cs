7151b4edd1c426416de21c3b3362f2a7e2507bc6
ramabit <f.e.ramirez94@gmail.com>
2014-05-30 22:46:42 -0300

    Agregado en login decision segun cantidad de roles
    Si el usuario tiene mas de un rol se va al form ElegirRol, si tiene solo
    1 rol va directamente a MenuPrincipal

--- FrbaCommerce/FrbaCommerce/Login/LoginForm.cs
+++ FrbaCommerce/FrbaCommerce/Login/LoginForm.cs
@@ -39,6 +39,24 @@ namespace FrbaCommerce.Login
             if (reader.Read())
             {
                 MessageBox.Show("Bienvenido " + reader["username"] + "!");
+                
+                parametros.Clear();
+                parametros.Add(new SqlParameter("@username", this.textBoxUsuario.Text));
+
+                String consulta = "select count(rol_id) from Rol_x_Usuario where habilitado = 1 and (select id from Usuario where username = @username) = usuario_id";
+                int cantidadDeRoles = (int)builderDeComandos.Crear(consulta, parametros).ExecuteScalar();
+
+                if(cantidadDeRoles > 1)
+                {
+                    new ElegirRol().Show();
+                    this.Hide();
+                }
+                else
+                {
+                    new MenuPrincipal().Show();
+                    this.Hide();
+                }
+
             }
             else
             {
---
+++ FrbaCommerce/FrbaCommerce/MenuPrincipal.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace FrbaCommerce
+{
+    public partial class MenuPrincipal : Form
+    {
+        public MenuPrincipal()
+        {
+            InitializeComponent();
+        }
+    }
+}
