9de5b311fb164d4a4897de519f5006c4531b77c1
Gonzalo-F <gonzalo.frc@gmail.com>
2014-05-31 18:58:00 -0300

    Agrego - Agregar Rol

--- FrbaCommerce/FrbaCommerce/ABM Rol/AgregarRol.Designer.cs
+++ FrbaCommerce/FrbaCommerce/ABM Rol/AgregarRol.Designer.cs
@@ -29,11 +29,17 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 203);
+            this.button1.Location = new System.Drawing.Point(32, 320);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(122, 23);
             this.button1.TabIndex = 0;
@@ -41,20 +47,89 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(132, 77);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(193, 199);
+            this.checkedListBox1.TabIndex = 1;
+            this.checkedListBox1.SelectedIndexChanged += new System.EventHandler(this.checkedListBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(80, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Rol";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(132, 28);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(94, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(29, 77);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Funcionalidades";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(229, 334);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(96, 30);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Guardar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(230, 293);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(95, 26);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Limpiar";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
             // AgregarRol
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.ClientSize = new System.Drawing.Size(351, 376);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkedListBox1);
             this.Controls.Add(this.button1);
             this.Name = "AgregarRol";
             this.Text = "AgregarRol";
+            this.Load += new System.EventHandler(this.AgregarRol_Load_1);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
--- FrbaCommerce/FrbaCommerce/ABM Rol/AgregarRol.cs
+++ FrbaCommerce/FrbaCommerce/ABM Rol/AgregarRol.cs
@@ -5,15 +5,37 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Data.SqlClient;
 using System.Windows.Forms;
 
 namespace FrbaCommerce.ABM_Rol
 {
     public partial class AgregarRol : Form
     {
+        private SqlCommand command { get; set; }
+        private IList<SqlParameter> parametros = new List<SqlParameter>();
+        private BuilderDeComandos builderDeComandos = new BuilderDeComandos();
+        
         public AgregarRol()
         {
             InitializeComponent();
+        }
+
+        private void AgregarRol_Load_1(object sender, EventArgs e)
+        {
+            CargarFuncionalidades();
+        }   
+
+        private void CargarFuncionalidades()
+        {
+            DataSet funcionalidades = new DataSet();
+            SqlDataAdapter adapter = new SqlDataAdapter();
+            parametros = new List<SqlParameter>();
+            command = builderDeComandos.Crear("SELECT distinct nombre FROM Funcionalidad", parametros);
+            adapter.SelectCommand = command;
+            adapter.Fill(funcionalidades);
+            checkedListBox1.DataSource = funcionalidades.Tables[0].DefaultView;
+            checkedListBox1.ValueMember = "nombre";
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -22,5 +44,47 @@ namespace FrbaCommerce.ABM_Rol
             new RolForm().Show();
             this.Close();
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            String sql = "INSERT INTO Rol(nombre, habilitado) VALUES (@rol, 1)";
+            parametros.Clear();
+            parametros.Add(new SqlParameter("@rol", this.textBox1.Text));
+            builderDeComandos.Crear(sql, parametros).ExecuteNonQuery();
+
+            foreach (DataRowView funcionalidad in this.checkedListBox1.CheckedItems)
+            {
+                parametros.Clear();
+                parametros.Add(new SqlParameter("@rol", this.textBox1.Text));
+
+                parametros.Add(new SqlParameter("@funcionalidad", funcionalidad.Row["nombre"] as String));
+                                                
+                String sql2 = "INSERT INTO Funcionalidad_x_Rol(funcionalidad_id, rol_id) VALUES ((SELECT id FROM Funcionalidad WHERE nombre = @funcionalidad), (SELECT  id FROM Rol WHERE nombre = @rol))";
+                                
+                builderDeComandos.Crear(sql2, parametros).ExecuteNonQuery();                                
+            }
+            MessageBox.Show("Se creo el rol " + this.textBox1.Text);
+            BorrarDatosIngresados();
+        }
+
+        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            BorrarDatosIngresados();            
+        }
+
+        private void BorrarDatosIngresados()
+        {
+            textBox1.Clear();
+            for (int i = 0; i < checkedListBox1.Items.Count; i++)
+            {
+                checkedListBox1.SetItemChecked(i, false);
+            }
+        }
+                     
     }
 }
