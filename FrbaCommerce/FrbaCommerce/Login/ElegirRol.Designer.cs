5f5defb3e11249733cad629a8e6cbda98bd8f30c
ramabit <f.e.ramirez94@gmail.com>
2014-05-31 21:14:39 -0300

    Agregando form de elegir rol si tiene mas de 1
    Ya despliega la lista de opciones de roles que puede elegir si tiene mas
    de 1

--- FrbaCommerce/FrbaCommerce/Login/ElegirRol.Designer.cs
+++ FrbaCommerce/FrbaCommerce/Login/ElegirRol.Designer.cs
@@ -28,11 +28,60 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.comboBoxRol = new System.Windows.Forms.ComboBox();
+            this.botonAceptar = new System.Windows.Forms.Button();
+            this.labelEnunciado = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // comboBoxRol
+            // 
+            this.comboBoxRol.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBoxRol.FormattingEnabled = true;
+            this.comboBoxRol.Location = new System.Drawing.Point(107, 70);
+            this.comboBoxRol.Name = "comboBoxRol";
+            this.comboBoxRol.Size = new System.Drawing.Size(145, 32);
+            this.comboBoxRol.TabIndex = 0;
+            // 
+            // botonAceptar
+            // 
+            this.botonAceptar.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.botonAceptar.Location = new System.Drawing.Point(107, 119);
+            this.botonAceptar.Name = "botonAceptar";
+            this.botonAceptar.Size = new System.Drawing.Size(145, 40);
+            this.botonAceptar.TabIndex = 1;
+            this.botonAceptar.Text = "Aceptar";
+            this.botonAceptar.UseVisualStyleBackColor = true;
+            // 
+            // labelEnunciado
+            // 
+            this.labelEnunciado.AutoSize = true;
+            this.labelEnunciado.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelEnunciado.Location = new System.Drawing.Point(12, 30);
+            this.labelEnunciado.Name = "labelEnunciado";
+            this.labelEnunciado.Size = new System.Drawing.Size(345, 20);
+            this.labelEnunciado.TabIndex = 2;
+            this.labelEnunciado.Text = "Seleccione el rol con el cual quiere iniciar sesion";
+            // 
+            // ElegirRol
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(365, 212);
+            this.Controls.Add(this.labelEnunciado);
+            this.Controls.Add(this.botonAceptar);
+            this.Controls.Add(this.comboBoxRol);
+            this.Name = "ElegirRol";
             this.Text = "ElegirRol";
+            this.Load += new System.EventHandler(this.ElegirRol_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ComboBox comboBoxRol;
+        private System.Windows.Forms.Button botonAceptar;
+        private System.Windows.Forms.Label labelEnunciado;
     }
 }
