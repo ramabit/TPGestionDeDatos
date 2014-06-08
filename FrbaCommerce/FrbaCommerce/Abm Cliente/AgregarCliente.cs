97a6c293667fafb7238d8bcb36f41d2be661dcb3
Santiago Perez Torre <saantii.28@hotmail.com>
2014-06-08 19:42:11 +0200

    Modificacion a agregar cliente
    Ahora carga los tipos de documentos al iniciar el form

--- FrbaCommerce/FrbaCommerce/Abm Cliente/AgregarCliente.cs
+++ FrbaCommerce/FrbaCommerce/Abm Cliente/AgregarCliente.cs
@@ -6,19 +6,41 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace FrbaCommerce.ABM_Cliente
 {
     public partial class AgregarCliente : Form
     {
-        public AgregarCliente(String usuario, String contraseña)
+        private BuilderDeComandos builderDeComandos = new BuilderDeComandos();
+        private String query;
+        private SqlCommand command;
+        private IList<SqlParameter> parametros = new List<SqlParameter>();
+        private String usuario;
+        private String contrasena;
+
+        public AgregarCliente(String usuario, String contrasena)
         {
             InitializeComponent();
+            this.usuario = usuario;
+            this.contrasena = contrasena;
         }
 
         private void AgregarCliente_Load(object sender, EventArgs e)
         {
+            CargarTipoDeDocumentos();
+        }
 
+        private void CargarTipoDeDocumentos()
+        {
+            command = builderDeComandos.Crear("SELECT nombre FROM LOS_SUPER_AMIGOS.TipoDeDocumento tipoDoc", parametros);
+
+            DataSet rubros = new DataSet();
+            SqlDataAdapter adapter = new SqlDataAdapter();
+            adapter.SelectCommand = command;
+            adapter.Fill(rubros);
+            comboBox_TipoDeDocumento.DataSource = rubros.Tables[0].DefaultView;
+            comboBox_TipoDeDocumento.ValueMember = "nombre";
         }
 
         private void button_Guardar_Click(object sender, EventArgs e)
