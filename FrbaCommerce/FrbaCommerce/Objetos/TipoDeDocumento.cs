13235e73af64b1ddccfaf443fd3da2a9626a3525
Santiago Perez Torre <saantii.28@hotmail.com>
2014-06-12 19:41:42 +0200

    Agrego una clase TipoDeDocumento
    es una data class por ahora

---
+++ FrbaCommerce/FrbaCommerce/Objetos/TipoDeDocumento.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FrbaCommerce.Objetos
+{
+    class TipoDeDocumento
+    {
+        private Decimal id;
+        private String nombre;
+
+        public TipoDeDocumento()
+        {
+            this.id = 0;
+            this.nombre = "";
+        }
+
+        public void SetId(Decimal id)
+        {
+            this.id = id;
+        }
+
+        public Decimal GetId()
+        {
+            return this.id;
+        }
+
+        public void SetNombre(String nombre)
+        {
+            this.nombre = nombre;
+        }
+
+        public String GetNombre()
+        {
+            return this.nombre;
+        }
+    }
+}
