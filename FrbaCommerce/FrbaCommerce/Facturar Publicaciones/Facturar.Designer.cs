eea6d87a0485769f9876d44c59e1fde215e80c6b
ramabit <f.e.ramirez94@gmail.com>
2014-06-12 18:02:07 -0300

    Comenzando facturar

--- FrbaCommerce/FrbaCommerce/Facturar Publicaciones/Facturar.Designer.cs
+++ FrbaCommerce/FrbaCommerce/Facturar Publicaciones/Facturar.Designer.cs
@@ -28,20 +28,72 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.labelCantCompras = new System.Windows.Forms.Label();
+            this.domainUpDown1 = new System.Windows.Forms.DomainUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.botonFacturar = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // labelCantCompras
+            // 
+            this.labelCantCompras.AutoSize = true;
+            this.labelCantCompras.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelCantCompras.Location = new System.Drawing.Point(22, 43);
+            this.labelCantCompras.Name = "labelCantCompras";
+            this.labelCantCompras.Size = new System.Drawing.Size(207, 17);
+            this.labelCantCompras.TabIndex = 0;
+            this.labelCantCompras.Text = "Cantidad de compras a facturar";
+            // 
+            // domainUpDown1
+            // 
+            this.domainUpDown1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.domainUpDown1.Location = new System.Drawing.Point(235, 41);
+            this.domainUpDown1.Name = "domainUpDown1";
+            this.domainUpDown1.Size = new System.Drawing.Size(120, 23);
+            this.domainUpDown1.TabIndex = 1;
+            this.domainUpDown1.Text = "0";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 160);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(339, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "*Recuerde que si acumula 10 compras sin facturar, será deshabilitado ";
+            // 
+            // botonFacturar
+            // 
+            this.botonFacturar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.botonFacturar.Location = new System.Drawing.Point(167, 94);
+            this.botonFacturar.Name = "botonFacturar";
+            this.botonFacturar.Size = new System.Drawing.Size(111, 37);
+            this.botonFacturar.TabIndex = 3;
+            this.botonFacturar.Text = "Facturar";
+            this.botonFacturar.UseVisualStyleBackColor = true;
             // 
             // Facturar
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(467, 223);
+            this.ClientSize = new System.Drawing.Size(437, 221);
+            this.Controls.Add(this.botonFacturar);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.domainUpDown1);
+            this.Controls.Add(this.labelCantCompras);
             this.Name = "Facturar";
-            this.Text = "Form1";
+            this.Text = "Facturar Publicaciones";
             this.Load += new System.EventHandler(this.Facturar_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label labelCantCompras;
+        private System.Windows.Forms.DomainUpDown domainUpDown1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button botonFacturar;
     }
 }
