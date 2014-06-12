c16c9bc3fd4989c69485d670617f6fd6a7b75e29
Santiago Perez Torre <saantii.28@hotmail.com>
2014-06-12 19:40:54 +0200

    Agrego una clase Adaptador con BD

---
+++ FrbaCommerce/FrbaCommerce/ComunicadorConBaseDeDatos.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+using FrbaCommerce.Objetos;
+
+namespace FrbaCommerce
+{
+    class ComunicadorConBaseDeDatos
+    {
+        private String query;
+        private IList<SqlParameter> parametros = new List<SqlParameter>();
+        private SqlParameter parametroOutput;
+        private SqlCommand command;
+        private BuilderDeComandos builderDeComandos = new BuilderDeComandos();
+
+        public Decimal ObtenerIdDe(TipoDeDocumento tipoDeDocumento)
+        {
+            query = "SELECT id FROM LOS_SUPER_AMIGOS.TipoDeDocumento WHERE nombre = @tipoDeDocumento";
+            parametros.Clear();
+            parametros.Add(new SqlParameter("@tipoDeDocumento", tipoDeDocumento.GetNombre()));
+            return (Decimal) builderDeComandos.Crear(query, parametros).ExecuteScalar();
+        }
+    }
+}
