76515c2bf826a4ec54325c2cf1472b867189b196
Santiago Perez Torre <saantii.28@hotmail.com>
2014-06-12 21:36:03 +0200

    Agrego clase Empresa

---
+++ FrbaCommerce/FrbaCommerce/Objetos/Empresa.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FrbaCommerce.Objetos
+{
+    class Empresa
+    {
+        private Decimal id;
+        private String razonSocial;
+        private String nombreDeContacto;
+        private String cuit;
+        private String fechaDeCreacion;
+        private String mail;
+        private String telefono;
+        private String ciudad;
+        private Decimal idDireccion;
+        private Decimal idUsuario;
+
+        public void SetId(Decimal id)
+        {
+            this.id = id;
+        }
+
+        public Decimal GetId()
+        {
+            return this.id;
+        }
+
+        public void SetRazonSocial(String razonSocial)
+        {
+            this.razonSocial = razonSocial;
+        }
+
+        public String GetRazonSocial()
+        {
+            return this.razonSocial;
+        }
+
+        public void SetNombreDeContacto(String nombreDeContacto)
+        {
+            this.nombreDeContacto = nombreDeContacto;
+        }
+
+        public String GetNombreDeContacto()
+        {
+            return this.nombreDeContacto;
+        }
+
+        public void SetCuit(String cuit)
+        {
+            this.cuit = cuit;
+        }
+
+        public String GetCuit()
+        {
+            return this.cuit;
+        }
+
+        public void SetFechaDeCreacion(String fechaDeCreacion)
+        {
+            this.fechaDeCreacion = fechaDeCreacion;
+        }
+
+        public String GetFechaDeCreacion()
+        {
+            return this.fechaDeCreacion;
+        }
+
+        public void SetCiudad(String ciudad)
+        {
+            this.ciudad = ciudad;
+        }
+
+        public String GetCiudad()
+        {
+            return this.ciudad;
+        }
+
+        public void SetMail(String mail)
+        {
+            this.mail = mail;
+        }
+
+        public String GetMail()
+        {
+            return this.mail;
+        }
+
+        public void SetTelefono(String telefono)
+        {
+            this.telefono = telefono;
+        }
+
+        public String GetTelefono()
+        {
+            return this.telefono;
+        }
+
+        public void SetIdDireccion(Decimal idDireccion)
+        {
+            this.idDireccion = idDireccion;
+        }
+
+        public Decimal GetIdDireccion()
+        {
+            return this.idDireccion;
+        }
+
+        public void SetIdUsuario(Decimal idUsuario)
+        {
+            this.idUsuario = idUsuario;
+        }
+
+        public Decimal GetIdUsuario()
+        {
+            return this.idUsuario;
+        }
+    }
+}
