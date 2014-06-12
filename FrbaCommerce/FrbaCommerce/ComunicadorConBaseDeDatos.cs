7026555d2b3f4ec08b70b639f2bd9b1203fa6346
Santiago Perez Torre <saantii.28@hotmail.com>
2014-06-12 20:07:14 +0200

    Agrego metodos al Adaptador
    Metodos:
    - obtenerNombreDe(TipoDeDocumento)
    - CrearDireccion(Direccion)

--- FrbaCommerce/FrbaCommerce/ComunicadorConBaseDeDatos.cs
+++ FrbaCommerce/FrbaCommerce/ComunicadorConBaseDeDatos.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Data.SqlClient;
 using FrbaCommerce.Objetos;
+using FrbaCommerce.Exceptions;
+using System.Data;
 
 namespace FrbaCommerce
 {
@@ -17,10 +19,47 @@ namespace FrbaCommerce
 
         public Decimal ObtenerIdDe(TipoDeDocumento tipoDeDocumento)
         {
+            if (tipoDeDocumento.GetNombre() == "") 
+                throw new FaltanDefinirAtributosException();
             query = "SELECT id FROM LOS_SUPER_AMIGOS.TipoDeDocumento WHERE nombre = @tipoDeDocumento";
             parametros.Clear();
             parametros.Add(new SqlParameter("@tipoDeDocumento", tipoDeDocumento.GetNombre()));
-            return (Decimal) builderDeComandos.Crear(query, parametros).ExecuteScalar();
+            Decimal idTipoDeDocumento = (Decimal)builderDeComandos.Crear(query, parametros).ExecuteScalar();
+            tipoDeDocumento.SetId(idTipoDeDocumento);
+            return idTipoDeDocumento;
+        }
+
+        public String ObtenerNombreDe(TipoDeDocumento tipoDeDocumento)
+        {
+            if (tipoDeDocumento.GetId() == 0)
+                throw new FaltanDefinirAtributosException();
+            query = "SELECT nombre FROM LOS_SUPER_AMIGOS.TipoDeDocumento WHERE id = @idTipoDeDocumento";
+            parametros.Clear();
+            parametros.Add(new SqlParameter("@idTipoDeDocumento", tipoDeDocumento.GetId()));
+            String nombreTipoDeDocumento = (String)builderDeComandos.Crear(query, parametros).ExecuteScalar();
+            tipoDeDocumento.SetNombre(nombreTipoDeDocumento);
+            return nombreTipoDeDocumento;
+        }
+
+        public Decimal CrearDireccion(Direccion direccion)
+        {
+            query = "LOS_SUPER_AMIGOS.crear_direccion";
+            parametros.Clear();
+            parametroOutput = new SqlParameter("@direccion_id", SqlDbType.Decimal);
+            parametroOutput.Direction = ParameterDirection.Output;
+            parametros.Add(new SqlParameter("@calle", direccion.GetCalle()));
+            parametros.Add(new SqlParameter("@numero", direccion.GetNumero()));
+            parametros.Add(new SqlParameter("@piso", direccion.GetPiso()));
+            parametros.Add(new SqlParameter("@depto", direccion.GetDepartamento()));
+            parametros.Add(new SqlParameter("@cod_postal", direccion.GetCodigoPostal()));
+            parametros.Add(new SqlParameter("@localidad", direccion.GetLocalidad()));
+            parametros.Add(parametroOutput);
+            command = builderDeComandos.Crear(query, parametros);
+            command.CommandType = CommandType.StoredProcedure;
+            command.ExecuteNonQuery();
+            Decimal idDireccionNueva = (Decimal)parametroOutput.Value;
+            direccion.SetId(idDireccionNueva);
+            return idDireccionNueva;
         }
     }
 }
