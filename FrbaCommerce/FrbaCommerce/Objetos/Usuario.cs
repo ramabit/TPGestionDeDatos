01cecf9c1ae0487ebc8b5412116b9584096c4b9d
Santiago Perez Torre <saantii.28@hotmail.com>
2014-06-12 20:44:25 +0200

    Agrego clase Usuario

---
+++ FrbaCommerce/FrbaCommerce/Objetos/Usuario.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FrbaCommerce.Objetos
+{
+    class Usuario
+    {
+        private Decimal id;
+        private String username;
+        private String password;
+
+        public void SetId(Decimal id)
+        {
+            this.id = id;
+        }
+
+        public Decimal GetId()
+        {
+            return this.id;
+        }
+
+        public void SetUsername(String username)
+        {
+            this.username = username;
+        }
+
+        public String GetUsername()
+        {
+            return this.username;
+        }
+
+        public void SetPassword(String password)
+        {
+            this.password = password;
+        }
+
+        public String GetPassword()
+        {
+            return this.password;
+        }
+    }
+}
