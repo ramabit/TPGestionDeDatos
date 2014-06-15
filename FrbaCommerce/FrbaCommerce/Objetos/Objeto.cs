f8a57b40899fb743b4d2563c0b952b59a38c2909
Santiago Perez Torre <saantii.28@hotmail.com>
2014-06-15 22:40:28 +0200

    Agrego objeto con nombre Objeto
    Es el padre de todos los objetos

---
+++ FrbaCommerce/FrbaCommerce/Objetos/Objeto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FrbaCommerce.Objetos
+{
+    class Objeto
+    {
+        public Boolean esNumero(String numString)
+        {
+            long number1 = 0;
+            return long.TryParse(numString, out number1); // devuelve true si pudo convertirlo, es decir, es numero
+        }
+
+        public Boolean esDouble(String numString)
+        {
+            Double number1 = 0;
+            return Double.TryParse(numString, out number1); // devuelve true si pudo convertirlo, es decir, es numero
+        }
+    }
+}
