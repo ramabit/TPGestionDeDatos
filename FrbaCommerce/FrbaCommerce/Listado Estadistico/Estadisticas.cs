54c94c800900a1044229e143c5d1d1cd3220cd14
Santiago Perez Torre <saantii.28@hotmail.com>
2014-06-17 01:20:38 +0200

    Gran avance en estadistica
    Ya se puede obtener las estadisticas de los ultimos 3

--- FrbaCommerce/FrbaCommerce/Listado Estadistico/Estadisticas.cs
+++ FrbaCommerce/FrbaCommerce/Listado Estadistico/Estadisticas.cs
@@ -11,6 +11,8 @@ namespace FrbaCommerce.Listado_Estadistico
 {
     public partial class Estadisticas : Form
     {
+        private ComunicadorConBaseDeDatos comunicador = new ComunicadorConBaseDeDatos();
+
         public Estadisticas()
         {
             InitializeComponent();
@@ -26,10 +28,10 @@ namespace FrbaCommerce.Listado_Estadistico
         {
             DataTable trimestres = new DataTable();
             trimestres.Columns.Add("trimestres");
-            trimestres.Rows.Add("1er trimestre (Enero - Marzo");
-            trimestres.Rows.Add("2do trimestre (Abril - Junio");
-            trimestres.Rows.Add("3er trimestre (Julio - Septiembre");
-            trimestres.Rows.Add("4to trimestre (Octubre - Diciembre");
+            trimestres.Rows.Add("1er trimestre (Enero - Marzo)");
+            trimestres.Rows.Add("2do trimestre (Abril - Junio)");
+            trimestres.Rows.Add("3er trimestre (Julio - Septiembre)");
+            trimestres.Rows.Add("4to trimestre (Octubre - Diciembre)");
             comboBox_Trimestre.DataSource = trimestres;
             comboBox_Trimestre.ValueMember = "trimestres";
             comboBox_Trimestre.SelectedIndex = -1;
@@ -54,24 +56,73 @@ namespace FrbaCommerce.Listado_Estadistico
             String trimestre = comboBox_Trimestre.Text;
             String tipoDeListado = comboBox_TipoDeListado.Text;
 
-            String queryParaObtenerResultados = GetQueryObtenerResultados(tipoDeListado);
+            String fechaDeInicio = ObtenerFechaDeInicio(anio, trimestre);
+            String fechaDeFin = ObtenerFechaDeFin(anio, trimestre);
+            String queryParaObtenerResultados = GetQueryObtenerResultados(tipoDeListado, fechaDeInicio, fechaDeFin);
+            
+            dataGridView_Estadistica.DataSource = comunicador.SelectDataTable("*", queryParaObtenerResultados);
         }
 
-        private string GetQueryObtenerResultados(String tipoDeListado)
+        private String ObtenerFechaDeInicio(string anio, string trimestre)
+        {
+            String dia = "01";
+            String mes = ObtenerMesInicio(trimestre);
+            return dia + "/" + mes + "/" + anio;
+        }
+
+        private string ObtenerMesInicio(string trimestre)
+        {
+            switch (trimestre[0])
+            {
+                case '1':
+                    return "01";
+                case '2':
+                    return "04";
+                case '3':
+                    return "07";
+                case '4':
+                    return "10";
+            }
+            throw new Exception("No pudo obtener mes");
+        }
+
+        private String ObtenerFechaDeFin(string anio, string trimestre)
+        {
+            String dia = "01";
+            String mes = ObtenerMesFin(trimestre);
+            return dia + "/" + mes + "/" + anio;
+        }
+
+        private string ObtenerMesFin(string trimestre)
+        {
+            switch (trimestre[0])
+            {
+                case '1':
+                    return "03";
+                case '2':
+                    return "06";
+                case '3':
+                    return "09";
+                case '4':
+                    return "12";
+            }
+            throw new Exception("No pudo obtener mes");
+        }
+
+        private string GetQueryObtenerResultados(String tipoDeListado, String fechaDeInicio, String fechaDeFin)
         {
             switch (tipoDeListado)
             {
                 case "Vendedores con mayor cantidad de productos no vendidos":
                     return "";
                 case "Vendedores con mayor facturacion":
-                    return "";
+                    return "LOS_SUPER_AMIGOS.vendedores_con_mayor_facturacion('" + fechaDeInicio + "', '" + fechaDeFin + "')";
                 case "Vendedores con mayores calificaciones":
-                    return "";
+                    return "LOS_SUPER_AMIGOS.vendedores_con_mayor_calificacion('" + fechaDeInicio + "', '" + fechaDeFin + "')";
                 case "Clientes con mayor cantidad de publicaciones sin calificar":
-                    return "";
-                default:
-                    return "";
+                    return "LOS_SUPER_AMIGOS.clientes_con_publicaciones_sin_calificar('" + fechaDeInicio + "', '" + fechaDeFin + "')";
             }
+            throw new Exception("No se pudo obtener la funcion");
         }
 
         private void button_Limpiar_Click(object sender, EventArgs e)
