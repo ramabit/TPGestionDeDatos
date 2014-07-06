b690454b1cc8939cb352ee0a708f29a4df2d764d
Santiago Perez Torre <saantii.28@hotmail.com>
2014-07-06 03:04:36 +0200

    Pequeñitos cambios en ver preguntas y respuestas
    Ya no se puede modificar
    No permiten agregar celdas

--- FrbaCommerce/FrbaCommerce/Gestion de Preguntas/VerPreguntas.Designer.cs
+++ FrbaCommerce/FrbaCommerce/Gestion de Preguntas/VerPreguntas.Designer.cs
@@ -36,9 +36,12 @@
             // 
             // dataGridView1
             // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Location = new System.Drawing.Point(23, 76);
             this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
             this.dataGridView1.Size = new System.Drawing.Size(323, 194);
             this.dataGridView1.TabIndex = 0;
             // 
--- FrbaCommerce/FrbaCommerce/Gestion de Preguntas/VerRespuestas.Designer.cs
+++ FrbaCommerce/FrbaCommerce/Gestion de Preguntas/VerRespuestas.Designer.cs
@@ -56,9 +56,12 @@
             // 
             // dataGridView1
             // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Location = new System.Drawing.Point(20, 69);
             this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
             this.dataGridView1.Size = new System.Drawing.Size(435, 189);
             this.dataGridView1.TabIndex = 2;
             // 
